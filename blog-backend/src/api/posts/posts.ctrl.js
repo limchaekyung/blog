56870383361eb179e005466f1f5941d1091603df
limchaekyung <corud505@naver.com>
2021-11-17 15:07:34 +0900

    페이지네이션

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -57,9 +57,30 @@ export const write = async ctx => {
 GET /api/posts
 */
 export const list = async ctx => {
+
+    // query는 문자열이기 때문에 숫자로 변환해주어야 한다.
+    // 값이 주어지지 않았다면 1을 기본으로 사용한다.
+    const page = parseInt(ctx.query.page || '1', 10);
+
+    if(page < 1){
+        ctx.status = 400;
+        return;
+    }
+    
     try{
-        const posts = await Post.find().exec();
-        ctx.body = posts;
+        const posts = await Post.find()
+        .sort({_id: -1})
+        .limit(10)
+        .skip((page - 1) * 10)
+        .lean()
+        .exec();
+        const postCount = await Post.countDocuments().exec();
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
+        ctx.body = posts.map(post => ({
+            ...post,
+            body:
+            post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+        }));
     }catch(e){
         ctx.throw(500, e);
     }
@@ -120,7 +141,7 @@ export const update = async ctx => {
         ctx.body = result.error;
         return;
     }
-    
+
     try{
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true,  // 이 값을 설정하면 업데이트된 데이터를 반환한다.
