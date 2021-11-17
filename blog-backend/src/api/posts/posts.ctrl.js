3ac322b8fedf835880ba8dac256e41673f58c841
limchaekyung <corud505@naver.com>
2021-11-17 17:49:30 +0900

    checkLoggedIn

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -4,10 +4,31 @@ import Joi from '@hapi/joi';
 
 const {ObjectId} = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
     const {id} = ctx.params;
     if(!ObjectId.isValid(id)){
         ctx.status = 400;   //Bad Request
+        return;
+    }
+    try{
+        const post = await Post.findById(id);
+
+        // 포스트가 존재하지 않을 때
+        if(!post){
+            ctx.status = 404;   // Not Found
+            return;
+        }
+        ctx.state.post = post;
+        return next();
+    }catch(e){
+        ctx.throw(500,e);
+    }
+}
+
+export const checkOwnPost = (ctx, next) => {
+    const {user, post} = ctx.state;
+    if(post.user._id.toString() !== user._id){
+        ctx.status = 403;
         return;
     }
     return next();
@@ -44,6 +65,7 @@ export const write = async ctx => {
         title, 
         body,
         tags,
+        user: ctx.state.user,
     });
     try{
         await post.save();
@@ -90,17 +112,7 @@ export const list = async ctx => {
 GET /api/posts/:id
 */
 export const read = async ctx => {
-    const {id} = ctx.params;
-    try{
-        const post = await Post.findById(id).exec();
-        if(!post){
-            ctx.status = 404;   //Not Found
-            return;
-        }
-        ctx.body = post;
-    }catch(e){
-        ctx.throw(500,e);
-    }
+    ctx.body = ctx.state.post;
 };
 
 /*
