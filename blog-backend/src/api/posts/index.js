1174570d0996236c40abe0fd970c07512ed9270d
limchaekyung <corud505@naver.com>
2021-11-19 04:13:42 +0900

    포스트 조회

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -7,10 +7,10 @@ const posts = new Router();
 posts.get('/', postsCtrl.list);
 posts.post('/', checkLoggedIn, postsCtrl.write);
 
-const post = new Router();  // /api/posts/:id
-posts.get('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.read);
-posts.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
-posts.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.getPostById, post.routes());
 
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -5,25 +5,24 @@ import Joi from '@hapi/joi';
 const {ObjectId} = mongoose.Types;
 
 export const getPostById = async (ctx, next) => {
-    const {id} = ctx.params;
-    if(!ObjectId.isValid(id)){
-        ctx.status = 400;   //Bad Request
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+      ctx.status = 400; // Bad Request
+      return;
+    }
+    try {
+      const post = await Post.findById(id);
+      // 포스트가 존재하지 않을 때
+      if (!post) {
+        ctx.status = 404; // Not Found
         return;
+      }
+      ctx.state.post = post;
+      return next();
+    } catch (e) {
+      ctx.throw(500, e);
     }
-    try{
-        const post = await Post.findById(id);
-
-        // 포스트가 존재하지 않을 때
-        if(!post){
-            ctx.status = 404;   // Not Found
-            return;
-        }
-        ctx.state.post = post;
-        return next();
-    }catch(e){
-        ctx.throw(500,e);
-    }
-}
+  };
 
 export const checkOwnPost = (ctx, next) => {
     const {user, post} = ctx.state;
